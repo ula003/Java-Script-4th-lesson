96c347bdcf57e6d5d2ba32819e0b419200078035
ula003 <xuyilajob.ru@gmail.com>
2024-05-01 01:34:21 +0500

    homework is ready

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-const arr = []
-
+const arr = [];
 const name = prompt('Name: ');
+const name2 = name.trim();
 
-if (name.length > 7 || name.endsWith(' ') ) {
-    const age = +prompt('Age: ');
-
-    if (age > 18) {
+if (name2.includes(' ')){
+    alert('Access denied')
+} else if(name.length >7){
+    const age = prompt('Age:')
+    if (age >= 18) {
         arr.push({
             name: name,
             age: age
         })
-    }
-} else {
-    alert('Access denied');
-}
-else {
-    alert('Access denied')git 
-}
-// let str = 'my name is ozodbek';
+    } else (alert('access denied'))
+} else (alert('access denied'))
+
