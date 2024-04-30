acef54378873213f6dca5f28ae0ee63da2b8fc9e
ula003 <xuyilajob.ru@gmail.com>
2024-05-01 00:16:38 +0500

    removed unnecessary items

--- main.js
+++ main.js
@@ -1,44 +1,3 @@
-// const a = +'15151';
-
-// console.log(isNaN(a));
-
-
-// const a = Number('a2');
-
-// console.log(a);
-
-
-// const a = parseFloat('1231.56')
-
-// console.log(a);
-
-
-// const a = parseInt('1231.56')
-
-// console.log(a);
-
-
-// Премитивные и Непремитивные
-
-// string, number, boolean, undefined, object, BigInt, Symbol, null
-
-// const a = {
-//     name: 'Ozodbek',
-//     age: 18,
-//     data_student_1: {
-//         name: 'Amir'
-//     }
-// };
-
-// console.log(a.name);
-// console.log(a.age);
-// console.log(a.data_student_1);
-// console.log(a['data_student_1']);
-
-
-// const array = [1, 'string', true, { key: 'value' }, undefined, null, Symbol, BigInt, [67, 234, 23, 4, 23, 42, 3, 4, 23, 42]];
-// array.push(56);
-// console.log(array);
 const arr = []
 
 const name = prompt('Name: ');
@@ -54,5 +13,8 @@ if (name.length > 7 || name.endsWith(' ') ) {
     }
 } else {
     alert('Access denied');
+}
+else {
+    alert('Access denied')git 
 }
 // let str = 'my name is ozodbek';
