884f013a96cd0299b8ea5025831e58c43b480b57
ula003 <xuyilajob.ru@gmail.com>
2024-05-01 00:08:44 +0500

    first commit

--- main.js
+++ main.js
@@ -1,12 +1,58 @@
-let str = "aaa@bbb@ccc"
-console.log( str.slice(0, 3) + "!" + str.slice(4, 7) + "!" + str.slice(8, 11) );
+// const a = +'15151';
 
-let trs = "HELLO WORLD";
-console.log(trs.slice(0, 1) + trs.slice(1).toLowerCase())
-
-const c = Math.random() * 10
-console.log(Math.round(c))
+// console.log(isNaN(a));
 
 
-const a = "Lex Luter has a big suit"
-console.log(a.charAt(11).toUpperCase() + a.slice(0, 3))
+// const a = Number('a2');
+
+// console.log(a);
+
+
+// const a = parseFloat('1231.56')
+
+// console.log(a);
+
+
+// const a = parseInt('1231.56')
+
+// console.log(a);
+
+
+// Премитивные и Непремитивные
+
+// string, number, boolean, undefined, object, BigInt, Symbol, null
+
+// const a = {
+//     name: 'Ozodbek',
+//     age: 18,
+//     data_student_1: {
+//         name: 'Amir'
+//     }
+// };
+
+// console.log(a.name);
+// console.log(a.age);
+// console.log(a.data_student_1);
+// console.log(a['data_student_1']);
+
+
+// const array = [1, 'string', true, { key: 'value' }, undefined, null, Symbol, BigInt, [67, 234, 23, 4, 23, 42, 3, 4, 23, 42]];
+// array.push(56);
+// console.log(array);
+const arr = []
+
+const name = prompt('Name: ');
+
+if (name.length > 7 || name.endsWith(' ') ) {
+    const age = +prompt('Age: ');
+
+    if (age > 18) {
+        arr.push({
+            name: name,
+            age: age
+        })
+    }
+} else {
+    alert('Access denied');
+}
+// let str = 'my name is ozodbek';
